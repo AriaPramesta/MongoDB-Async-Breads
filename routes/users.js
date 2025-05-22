40e8644988a2b1670de5104a371ca6888c4ee701
Aria Adi Pramesta <ariaapramesta@gmail.com>
2025-05-22 20:19:24 +0700

    Update: get data users API

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 module.exports = function (db) {
   const User = db.collection('users');
-  router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+  router.get('/', async function (req, res, next) {
+    try {
+      const users = await User.find({}).toArray();
+      res.status(200).json(users)
+    } catch (error) {
+      res.status(500).json({ message: error.message })
+    }
   });
 
   return router;
