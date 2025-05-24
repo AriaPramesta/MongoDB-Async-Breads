23c5149b91ff12669c546c1109bde3b2ef10dc1f
Aria Adi Pramesta <ariaapramesta@gmail.com>
2025-05-24 10:34:28 +0700

    Update: create API Endpoint todos update

--- routes/todos.js
+++ routes/todos.js
@@ -15,6 +15,25 @@ module.exports = function (db) {
         }
     });
 
+    router.get('/:id', async (req, res) => {
+        try {
+            const { userId, id } = req.params;
+
+            const todo = await Todos.findOne({
+                _id: new ObjectId(id),
+                userId: new ObjectId(userId)
+            });
+
+            if (!todo) {
+                return res.status(404).json({ message: 'Todo not found' });
+            }
+
+            res.status(200).json(todo);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    });
+
     router.post('/', async (req, res) => {
         try {
             const { title } = req.body
@@ -40,6 +59,18 @@ module.exports = function (db) {
 
         } catch (e) {
             res.status(500).json({ message: e.message })
+        }
+    })
+
+    router.put('/:id', async function (req, res, next) {
+        try {
+            const { id } = req.params
+            const _id = new ObjectId(id)
+            await Todos.updateOne({ _id }, { $set: req.body });
+            const todo = await Todos.findOne({ _id })
+            res.status(201).json(todo)
+        } catch (error) {
+            res.status(500).json({ message: error.message })
         }
     })
 
